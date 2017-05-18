6a8be844585c0afb57d9d46b31dd661538591fb1
AnneGoncalo <annegoncalo@gmail.com>
2017-05-17 23:09:53 -0300

    Ajuste SeNaoSe

---
+++ src/plp/enquanto/Principal.java
@@ -0,0 +1,62 @@
+package plp.enquanto;
+
+import java.io.IOException;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+import plp.enquanto.parser.EnquantoLexer;
+import plp.enquanto.parser.EnquantoParser;
+import plp.enquanto.parser.MeuListener;
+import static plp.enquanto.linguagem.Linguagem.*;
+import static java.util.Arrays.*;
+
+public class Principal {
+
+	private static ParseTree parse(String programa) {
+		final ANTLRInputStream input = new ANTLRInputStream(programa);
+		final EnquantoLexer lexer = new EnquantoLexer(input);
+		final CommonTokenStream tokens = new CommonTokenStream(lexer);
+		final EnquantoParser parser = new EnquantoParser(tokens);
+		return parser.programa();
+	}
+
+	public static void main(String... args) throws IOException {
+//		String programa = "x:=10; y:=leia ; c:= x + y; "
+//				+ "se 30 >= c entao escreva c senao exiba \"menor\"";
+		String programa = "x:=10; y:=leia; "
+				+ "se y = x entao exiba \"igual\" senaose y >= x entao exiba \"senaose maior\" senao exiba \"senao menor\"";
+//		String programa = "x:=10; "
+//				+ "escolha x caso 6 : exiba \"passou\" padrao exiba \"rodou\"";
+//		String programa = "para i de 1 ate 10 faca passo 2 { escreva i; }";
+//		String programa = "x:=10; y:=leia ; "
+//				+ "se x = y entao exiba \"igual\" senaose y<=x exiba \"maior\" senaose y<=20 exiba \"maior20\" senao exiba \"menor\";";
+		
+		final ParseTree tree = parse(programa);
+		final ParseTreeWalker walker = new ParseTreeWalker();
+		final MeuListener listener = new MeuListener();
+		walker.walk(listener, tree);
+		listener.getPrograma().execute();
+		
+//		Programa p1 = listener.getPrograma();
+//		// O parser devolve um objeto 'Programa' semelhante ao programa a seguir:
+//		Programa p2 = new Programa(asList(
+//				new Atribuicao("x", new Inteiro(10)),                       // x := 10
+//				new Atribuicao("y", leia),                                  // y := leia
+//				new Atribuicao("c", new ExpSoma(new Id("x"), new Id("y"))), // c := x + y
+//				new Se(new ExpMenorIgual(new Inteiro(30), new Id("c")),     // se 30 <= c entao
+//						new Escreva(new Id("c")),                           // escreva c
+//						new Exiba("menor"))                                 // senao exiba "menor"
+//				));
+////		Atribuicao x = new Atribuicao("x", new Inteiro(10));                       // x := 10
+////		Atribuicao y = new Atribuicao("y", leia);                                  // y := leia
+////		Se se = new Se(new ExpIgual(new Id("x"), new Id("y")),     			// se 30 <= c entao
+////				new Exiba("igual"), new Exiba("menor")) ;            // senao exiba "menor"
+////		se.AddSeNaoSe(new SeNaoSe(new ExpMaiorIgual(new Id("x"), new Id("y")) , new Exiba("maior")));
+////		Programa p2 = new Programa(asList(x, y, se) );
+//		p1.execute();
+//		p2.execute();
+	}
+}
---
+++ src/plp/enquanto/linguagem/Linguagem.java
@@ -0,0 +1,474 @@
+package plp.enquanto.linguagem;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public interface Linguagem {
+	final Map<String, Integer> ambiente = new HashMap<>();
+	final Scanner scanner = new Scanner(System.in);
+
+	interface Bool {
+		public boolean getValor();
+	}
+
+	interface Comando {
+		public void execute();
+	}
+
+	interface Expressao {
+		public int getValor();
+	}
+
+	abstract class ExpBin implements Expressao {
+		protected Expressao esq;
+		protected Expressao dir;
+
+		public ExpBin(Expressao esq, Expressao dir) {
+			this.esq = esq;
+			this.dir = dir;
+		}
+	}
+
+	class Programa {
+		private List<Comando> comandos;
+		public Programa(List<Comando> comandos) {
+			this.comandos = comandos;
+		}
+		public void execute() {
+			for (Comando comando : comandos) {
+				comando.execute();
+			}
+		}
+	}
+	
+	class SeNaoSe implements Comando {		//q08
+		private Bool condicao;
+		private Comando faca;
+		
+		public SeNaoSe(Bool condicao, Comando faca)
+		{
+			this.condicao = condicao;
+			this.faca = faca;
+		}
+		
+		@Override
+		public void execute(){
+			faca.execute();
+		}
+	}
+
+	class Se implements Comando {
+		private Bool condicao;
+		private Comando entao;
+		private Comando senao;
+		private List<SeNaoSe> opcoes;
+
+		public Se(Bool condicao, Comando entao, Comando senao) {
+			this.condicao = condicao;
+			this.entao = entao;
+			this.senao = senao;
+			this.opcoes = new ArrayList<>();
+		}
+		
+		public void AddSeNaoSe(SeNaoSe se){
+			opcoes.add(se);
+		}
+
+		@Override
+		public void execute() {
+			if (condicao.getValor()){
+				entao.execute();
+			}
+			else if(!opcoes.isEmpty()){		//q08
+				for(SeNaoSe se: opcoes){
+					if(se.condicao.getValor()){
+						se.execute();
+						return;
+					}
+				}
+				senao.execute();
+			}
+			else
+				senao.execute();
+		}
+	}
+
+	Skip skip = new Skip();
+	class Skip implements Comando {
+		@Override
+		public void execute() {
+		}
+	}
+
+	class Escreva implements Comando {
+		private Expressao exp;
+
+		public Escreva(Expressao exp) {
+			this.exp = exp;
+		}
+
+		@Override
+		public void execute() {
+			System.out.println(exp.getValor());
+		}
+	}
+
+	class Enquanto implements Comando {
+		private Bool condicao;
+		private Comando faca;
+
+		public Enquanto(Bool condicao, Comando faca) {
+			this.condicao = condicao;
+			this.faca = faca;
+		}
+
+		@Override
+		public void execute() {
+			while (condicao.getValor()) {
+				faca.execute();
+			}
+		}
+	}
+	
+	class Para implements Comando {		//q07
+		private String id;
+		private Expressao de;
+		private Expressao ate;
+		private Expressao passo;
+		private Comando faca;
+
+		public Para(String id, Expressao de, Expressao ate, Expressao passo, Comando faca) {
+			this.id = id;
+			this.de = de;
+			this.ate = ate;
+			this.passo = passo;
+			this.faca = faca;
+		}
+
+		@Override
+		public void execute() {
+			if(de.getValor() < ate.getValor()){
+				for(ambiente.put(id, de.getValor()); ambiente.get(id) < ate.getValor(); ambiente.put(id, ambiente.get(id)+passo.getValor())){
+					faca.execute();
+				}
+			}
+			else{
+				for(ambiente.put(id, de.getValor()); ambiente.get(id) > ate.getValor(); ambiente.put(id, ambiente.get(id)+passo.getValor())){
+					faca.execute();
+				}
+			}
+			ambiente.remove(id);
+		}
+	}
+	
+	class Caso implements Comando {		//q09
+		private Expressao opcao;
+		private Comando faca;
+		
+		public Caso(Expressao opcao, Comando faca){
+			this.opcao = opcao;
+			this.faca = faca;
+		}
+		
+		@Override
+		public void execute(){
+			faca.execute();
+		}
+	}
+	
+	class Escolha implements Comando {		//q09
+		private Expressao variavel;
+		private List<Caso> opcoes;
+		private Comando padrao;
+		
+		public Escolha(Expressao variavel, Comando padrao){
+			this.variavel = variavel;
+			opcoes = new ArrayList<>();
+			this.padrao = padrao;
+		}
+		
+		public void AddCaso(Caso caso){
+			opcoes.add(caso);
+		}
+		
+		@Override
+		public void execute(){
+			for(Caso c: opcoes){
+				if(c.opcao.getValor() == variavel.getValor()){
+					c.faca.execute();
+					return;
+				}
+			}
+			padrao.execute();
+		}
+	}
+
+	class Exiba implements Comando {
+		public Exiba(String texto) {
+			this.texto = texto;
+		}
+
+		private String texto;
+
+		@Override
+		public void execute() {
+			System.out.println(texto);
+		}
+	}
+
+	class Bloco implements Comando {
+		private List<Comando> comandos;
+
+		public Bloco(List<Comando> comandos) {
+			this.comandos = comandos;
+		}
+
+		@Override
+		public void execute() {
+			for (Comando comando : comandos) {
+				comando.execute();
+			}
+		}
+	}
+
+	class Atribuicao implements Comando {
+		private String id;
+		private Expressao exp;
+
+		public Atribuicao(String id, Expressao exp) {
+			this.id = id;
+			this.exp = exp;
+		}
+
+		@Override
+		public void execute() {
+			ambiente.put(id, exp.getValor());
+		}
+	}
+
+	class Inteiro implements Expressao {
+		private int valor;
+
+		public Inteiro(int valor) {
+			this.valor = valor;
+		}
+
+		@Override
+		public int getValor() {
+			return valor;
+		}
+	}
+
+	class Id implements Expressao {
+		private String id;
+
+		public Id(String id) {
+			this.id = id;
+		}
+
+		@Override
+		public int getValor() {
+			final Integer v = ambiente.get(id);
+			final int valor;
+			if (v != null)
+				valor = v;
+			else
+				valor = 0;
+
+			return valor;
+		}
+	}
+
+	Leia leia = new Leia();
+	class Leia implements Expressao {
+		@Override
+		public int getValor() {
+			return scanner.nextInt();
+		}
+	}
+
+	class ExpSoma extends ExpBin {
+		public ExpSoma(Expressao esq, Expressao dir) {
+			super(esq, dir);
+		}
+
+		@Override
+		public int getValor() {
+			return esq.getValor() + dir.getValor();
+		}
+	}
+
+	class ExpSub extends ExpBin {
+		public ExpSub(Expressao esq, Expressao dir) {
+			super(esq, dir);
+		}
+
+		@Override
+		public int getValor() {
+			return esq.getValor() - dir.getValor();
+		}
+	}
+	
+	class ExpMult extends ExpBin {
+		public ExpMult(Expressao esq, Expressao dir) {
+			super(esq, dir);
+		}
+
+		@Override
+		public int getValor() {
+			return esq.getValor() * dir.getValor();
+		}
+	}
+	
+	class ExpDivi extends ExpBin {		//q01
+		public ExpDivi(Expressao esq, Expressao dir) {
+			super(esq, dir);
+		}
+
+		@Override
+		public int getValor() {
+			return esq.getValor() / dir.getValor();
+		}
+	}
+	
+	class ExpExpo extends ExpBin {		//q02
+		public ExpExpo(Expressao esq, Expressao dir) {
+			super(esq, dir);
+		}
+
+		@Override
+		public int getValor() {
+			return esq.getValor() ^ dir.getValor();
+		}
+	}
+
+	class Booleano implements Bool {
+		private boolean valor;
+
+		public Booleano(boolean valor) {
+			this.valor = valor;
+		}
+
+		@Override
+		public boolean getValor() {
+			return valor;
+		}
+	}
+
+	abstract class ExpRel implements Bool {
+		protected Expressao esq;
+		protected Expressao dir;
+
+		public ExpRel(Expressao esq, Expressao dir) {
+			this.esq = esq;
+			this.dir = dir;
+		}
+	}
+
+	public class ExpIgual extends ExpRel {
+
+		public ExpIgual(Expressao esq, Expressao dir) {
+			super(esq, dir);
+		}
+
+		@Override
+		public boolean getValor() {
+			return esq.getValor() == dir.getValor();
+		}
+
+	}
+
+	public class ExpMenorIgual extends ExpRel {
+		public ExpMenorIgual(Expressao esq, Expressao dir) {
+			super(esq, dir);
+		}
+
+		@Override
+		public boolean getValor() {
+			return esq.getValor() <= dir.getValor();
+		}
+	}
+	
+	public class ExpMaiorIgual extends ExpRel {		//q05
+		public ExpMaiorIgual(Expressao esq, Expressao dir) {
+			super(esq, dir);
+		}
+
+		@Override
+		public boolean getValor() {
+			return esq.getValor() >= dir.getValor();
+		}
+	}
+	
+	public class ExpDiferente extends ExpRel {		//q06
+		public ExpDiferente(Expressao esq, Expressao dir) {
+			super(esq, dir);
+		}
+
+		@Override
+		public boolean getValor() {
+			return esq.getValor() != dir.getValor();
+		}
+	}
+
+	public class NaoLogico implements Bool {
+		private Bool b;
+
+		public NaoLogico(Bool b) {
+			this.b = b;
+		}
+
+		@Override
+		public boolean getValor() {
+			return !b.getValor();
+		}
+	}
+
+	public class ELogico implements Bool {
+		private Bool esq;
+		private Bool dir;
+
+		public ELogico(Bool esq, Bool dir) {
+			this.esq = esq;
+			this.dir = dir;
+		}
+
+		@Override
+		public boolean getValor() {
+			return esq.getValor() && dir.getValor();
+		}
+	}
+	
+	public class OuLogico implements Bool {	//q03
+		private Bool esq;
+		private Bool dir;
+
+		public OuLogico(Bool esq, Bool dir) {
+			this.esq = esq;
+			this.dir = dir;
+		}
+
+		@Override
+		public boolean getValor() {
+			return esq.getValor() || dir.getValor();
+		}
+	}
+	
+	public class XorLogico implements Bool {	//q04
+		private Bool esq;
+		private Bool dir;
+
+		public XorLogico(Bool esq, Bool dir) {
+			this.esq = esq;
+			this.dir = dir;
+		}
+
+		@Override
+		public boolean getValor() {
+			return (!esq.getValor() && dir.getValor()) || (esq.getValor() && !dir.getValor());
+		}
+	}
+}
